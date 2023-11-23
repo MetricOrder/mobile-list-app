10995ff7941f516613a885522e58c55762682ce8
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-11-23 14:08:44 +1100

    added firebase import and const to link appsettings argument

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
 const appSettings = {
     databaseURL: "https://listit-53c33-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
-
-
+const app = initializeApp(appSettings)
 const addBtnEl = document.querySelector("#add-button")
 const inputFieldEl = document.querySelector("#input-field")
 
