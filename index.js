967d8a7a1460d8bdf46b04056171dc931533706f
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-11-27 14:00:38 +1100

    added import function from firebase, added variables to hold info from firebase, coded button function to grab input values and store in firebase DB

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://listit-53c33-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemsInDB = ref(database, "items")
+
 const addBtnEl = document.querySelector("#add-button")
 const inputFieldEl = document.querySelector("#input-field")
 
-addBtn.addEventListener("click", function(){
+addBtnEl.addEventListener("click", function(){
     let inputValue = inputFieldEl.value
-
-    console.log(inputValue)    
+    push(itemsInDB, inputValue)
+    inputFieldEl = ""   
 })
