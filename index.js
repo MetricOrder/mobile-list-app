1fdffe5db39cad23a49d16d05ea113845b962fcd
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-11-28 14:28:54 +1100

    added variables for app, database and list info

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://lister-fd263-default-rtdb.asia-southeast1.firebasedatabase.app/"
 } 
+
+const app = appSettings(initializeApp)
+const database = getDatabase(app)
+const itemsInDB = ref(database, "itemsList")
+
 const addBtnEl = document.querySelector("#add-button")
 const inputFieldEl = document.querySelector("#input-field")
 
