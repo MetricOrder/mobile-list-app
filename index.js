6f5b1d3eeb88f88528b9370bf0d374c83a22c950
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-11-28 14:14:51 +1100

    deleted database. removed all database info, placed on line 50 - going to do it all again as a practice

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const appSettings = {
-    databaseURL: "https://listit-53c33-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const itemsInDB = ref(database, "items")
 
 const addBtnEl = document.querySelector("#add-button")
 const inputFieldEl = document.querySelector("#input-field")
 
 addBtnEl.addEventListener("click", function(){
     let inputValue = inputFieldEl.value
-    push(itemsInDB, inputValue)
-    inputFieldEl = ""   
+
+      
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+// import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+// const appSettings = {
+//     databaseURL: "https://listit-53c33-default-rtdb.asia-southeast1.firebasedatabase.app/"
+// }
+// const app = initializeApp(appSettings)
+// const database = getDatabase(app)
+// const itemsInDB = ref(database, "items")
+
+// push(itemsInDB, inputValue)
