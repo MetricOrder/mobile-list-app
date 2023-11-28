a6f00d980a002a8151fd164879a9852d2cd38867
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-11-28 14:39:58 +1100

    removed old comments

--- index.js
+++ index.js
@@ -19,14 +19,3 @@ addBtnEl.addEventListener("click", function(){
 
       
 })
-// import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-// import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-// const appSettings = {
-//     databaseURL: "https://listit-53c33-default-rtdb.asia-southeast1.firebasedatabase.app/"
-// }
-// const app = initializeApp(appSettings)
-// const database = getDatabase(app)
-// const itemsInDB = ref(database, "items")
-
-// push(itemsInDB, inputValue)
